cdeb8a798c9837d69c8ed5ec5f4e1965707d67da
nasjones <nasjonez1@gmail.com>
2021-04-01 12:54:56 -0400

    updated to use more const and change to css

--- connect4.js
+++ connect4.js
@@ -5,75 +5,64 @@
  * board fills (tie)
  */
 
-const DEFAULTWIDTH = 7;
-const DEFAULTHEIGHT = 6;
+const DEFAULT_WIDTH = 7;
+const DEFAULT_HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
-let board = []; // array of rows, each row is array of cells  (board[y][x])
+const BOARD = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
-function makeBoard(height, width) {
-	/* These lines check if a value was passed into the array
-	 if not the height and width are set to the default values */
-	if (!height) height = DEFAULTHEIGHT;
-	if (!width) width = DEFAULTWIDTH;
-	for (let i = 0; i < height; i++) board.push(new Array(width).fill(null));
+function makeBoard(height = DEFAULT_HEIGHT, width = DEFAULT_WIDTH) {
+	for (let i = 0; i < height; i++) {
+		BOARD.push(new Array(width).fill(null));
+	}
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-	const htmlBoard = document.getElementById("board");
+	const HTML_BOARD = document.getElementById("board");
 
 	/* this area of code creates the top row of the game board
 	that will let the user see which row they are currently 
 	aiming at */
-	let top = document.createElement("tr");
-	top.setAttribute("id", "column-top");
-	top.addEventListener("click", handleClick);
+	const TOP = document.createElement("tr");
+	TOP.setAttribute("id", "column-top");
+	TOP.addEventListener("click", handleClick);
 
 	/* this creates the columns for the top row that the game pieces 
 	will be placed in */
-	for (let x = 0; x < board[0].length; x++) {
-		let headCell = document.createElement("td");
-		headCell.setAttribute("id", x);
-		top.append(headCell);
+	for (let x = 0; x < BOARD[0].length; x++) {
+		const HEAD_CELL = document.createElement("td");
+		HEAD_CELL.setAttribute("id", x);
+		TOP.append(HEAD_CELL);
 	}
-	htmlBoard.append(top);
+	HTML_BOARD.append(TOP);
 
 	/* this code creates the squares for each row of the 
 	current column to fill in the rest of the game board */
-	for (let y = 0; y < board.length; y++) {
-		const row = document.createElement("tr");
-		for (let x = 0; x < board[0].length; x++) {
-			const cell = document.createElement("td");
-			cell.setAttribute("id", `${y}-${x}`);
-			cell.classList.add(`cell`);
-			row.append(cell);
+	for (let y = 0; y < BOARD.length; y++) {
+		const ROW = document.createElement("tr");
+		for (let x = 0; x < BOARD[0].length; x++) {
+			const CELL = document.createElement("td");
+			CELL.setAttribute("id", `${y}-${x}`);
+			CELL.classList.add(`cell`);
+			ROW.append(CELL);
 		}
-		htmlBoard.append(row);
+		HTML_BOARD.append(ROW);
 	}
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-	// TODO: write the real version of this, rather than always returning 0
-
-	// console.log(
-	// 	document.getElementsByClassName(`col-${x}`),
-	// 	Array.prototype.slice
-	// 		.call(document.getElementsByClassName(`col-${x}`))
-	// 		.map((cell) => cell.innerText),
-	// 	board
-	// );
-	console.log(board);
-	for (let i = board.length - 1; i >= 0; i--) {
-		if (!board[i][x]) return i;
-		// console.log(board[i][x]);
+	for (let i = BOARD.length - 1; i >= 0; i--) {
+		if (!BOARD[i][x]) {
+			return i;
+		}
 	}
 
 	return null;
@@ -82,17 +71,19 @@ function findSpotForCol(x) {
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-	let newPiece = document.createElement("div");
-	newPiece.classList.add("piece", `p${currPlayer}`, `row${y}`);
-	document
-		.getElementById("board")
-		.children[y + 1].children[x].append(newPiece);
+	const NEW_PIECE = document.createElement("div");
+	NEW_PIECE.classList.add("piece", `p${currPlayer}`);
+	const START_POS = -y * 100 - 150;
+	NEW_PIECE.style.top = `${START_POS}%`;
+	const BOARDDOM = document.getElementById("board");
+	const ROW = BOARDDOM.children[y + 1];
+	const TILE = ROW.children[x];
+	TILE.append(NEW_PIECE);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-	// TODO: pop up alert message
 	alert(msg);
 }
 
@@ -104,23 +95,24 @@ function handleClick(evt) {
 
 	// get next spot in column (if none, ignore click)
 	let y = findSpotForCol(x);
-	if (y === null) {
-		return;
+	if (y) {
+		// place piece in board and add to HTML table
+		// TODO: add line to update in-memory board
+		placeInTable(y, x);
+		BOARD[y][x] = currPlayer;
+
+		// check for win
+		const GAME_STATUS = checkForWin();
+		if (checkForWin()) {
+			endGame(`Player ${currPlayer} won!`);
+		} // check for tie
+		else if (checkForFull()) {
+			endGame(`It's a draw!`);
+		}
+
+		// switch players
+		currPlayer = currPlayer == 1 ? 2 : 1;
 	}
-
-	// place piece in board and add to HTML table
-	// TODO: add line to update in-memory board
-	placeInTable(y, x);
-	board[y][x] = currPlayer;
-
-	// check for win
-	if (checkForWin()) return endGame(`Player ${currPlayer} won!`);
-
-	// check for tie
-	if (!checkForWin() && checkForFull()) return endGame(`It's a draw!`);
-
-	// switch players
-	currPlayer = currPlayer == 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -133,18 +125,18 @@ function checkForWin() {
 		return cells.every(
 			([y, x]) =>
 				y >= 0 &&
-				y < board.length &&
+				y < BOARD.length &&
 				x >= 0 &&
-				x < board[0].length &&
-				board[y][x] === currPlayer
+				x < BOARD[0].length &&
+				BOARD[y][x] === currPlayer
 		);
 	}
 
 	// TODO: read and understand this code. Add comments to help you.
 	// loops through each row of the array/matrix
-	for (let y = 0; y < board[0].length; y++) {
+	for (let y = 0; y < BOARD[0].length; y++) {
 		// loops each item of the array/matrix
-		for (let x = 0; x < board[1].length; x++) {
+		for (let x = 0; x < BOARD[1].length; x++) {
 			// this creates an array of horizontal pieces
 			let horiz = [
 				[y, x],
@@ -182,7 +174,7 @@ function checkForWin() {
 }
 
 const checkForFull = () => {
-	return board.every((row) => row.every((cell) => cell !== null));
+	return BOARD.every((row) => row.every((cell) => cell !== null));
 };
 
 makeBoard();
